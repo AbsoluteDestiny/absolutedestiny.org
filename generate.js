656f5606f61dcf20f2c0a2bd2526df8af9370155
AbsoluteDestiny <ian@absolutedestiny.org>
2018-04-13 17:24:37 -0500

    chore: Pre-define critical css as build-time is failing

--- generate.js
+++ generate.js
@@ -2,12 +2,10 @@
 
 const buildJS = require('./build-js');
 const buildMetalsmith = require('./build-metalsmith.js');
-const critical = require('./critical.js');
 
 async function main() {
   await buildJS();
   await buildMetalsmith();
-  critical();
 };
 
 main();
