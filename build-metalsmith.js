9d9bc342d11f22d6f8bfd777a8f129a40e0c5ebb
AbsoluteDestiny <ian@absolutedestiny.org>
2018-04-28 19:08:57 -0500

    vid: Add vid Twilight

--- build-metalsmith.js
+++ build-metalsmith.js
@@ -42,206 +42,142 @@ const nextId = 0;
 async function buildMetalsmith() {
   return new Promise((resolve, reject) => {
     Metalsmith(process.cwd())
-    .metadata({
-      site: sitedata
-    })
-    .source("./src")
-    .destination("./build")
-    .use(timer("clean"))
-    .clean(true)
-    .use(ignore(["**/*.m4v"]))
-    .use(timer("less"))
-    .use(less())
-    .use(timer("autoprefixer"))
-    .use(autoprefixer())
-    .use(timer("css move"))
-    .use(
-      copy({
-        pattern: "assets/css/*.css",
-        move: false,
-        transform(file) {
-          return path.join(
-            ...path
-              .dirname(file)
-              .split(path.sep)
-              .slice(1),
-            "admin",
-            path.basename(file)
-          );
-        }
-      })
-    )
-    .use(timer("move other assets"))
-    .use(
-      copy({
-        pattern: "assets/**/*.*",
-        move: true,
-        transform(file) {
-          return path.join(
-            ...path
-              .dirname(file)
-              .split(path.sep)
-              .slice(1),
-            path.basename(file)
-          );
-        }
-      })
-    )
-    .use(cleanCSS())
-    .use(timer("fingerprint"))
-    .use(fingerprint({ pattern: ["css/*.css", "js/*.js"] }))
-    .use(timer("admin css"))
-    .use(
-      copy({
-        pattern: "css/admin/*.*",
-        move: true,
-        transform(file) {
-          return path.join("css", path.basename(file));
-        }
-      })
-    )
-    // .use(getFandoms())
-    .use(timer("admin config"))
-    .use(
-      replace({
-        "admin/config.yml": {
-          find: /abcd/gi,
-          replace: () => `${nextId + 1}`
-        }
-      })
-    )
-    .use(timer("rename md to basename version"))
-    .use(
-      each((f, filename) => {
-        const file = f;
-        file.basename = path.basename(filename, ".md");
-        return filename;
-      })
-    )
-    .use(ignore(["tags/**", "**/*.less"]))
-    .use(timer("copy vids to vidplayer"))
-    .use(
-      copy({
-        pattern: "vids/*.*",
-        move: false,
-        transform(file) {
-          const newpath = path.join(
-            "vidplayer",
-            ...path
-              .dirname(file)
-              .split(path.sep)
-              .slice(1),
-            path.basename(file)
-          );
-          return newpath;
-        }
-      })
-    )
-    .use(timer("add markdown layout metadata"))
-    .use(
-      fileMetadata([
-        { pattern: "*.md", metadata: { layout: "home.njk" } },
-        { pattern: "vids/**", metadata: { layout: "vid.njk" } },
-        { pattern: "vidplayer/**", metadata: { layout: "vidplayer.njk" } }
-      ])
-    )
-    .use(timer("make vids collection"))
-    .use(
-      collections({
-        vids: {
-          pattern: "vids/*.md",
-          sortBy: "date",
-          reverse: true
-        }
-      })
-    )
-    .use(timer("transform md to html"))
-    .use(markdown())
-    .use(timer("permalinks"))
-    .use(
-      permalinks({
-        // pattern: "post/:date/:title",
-        // date: "YYYY/MM/DD",
-
-        linksets: [
-          {
-            match: { collection: "vids" },
-            pattern: "vid/:basename"
-          },
-          {
-            match: { collection: "vidplayer" },
-            pattern: "vidplayer/:basename"
+      .metadata({ site: sitedata })
+      .source("./src")
+      .destination("./build")
+      .use(timer("clean"))
+      .clean(true)
+      .use(ignore(["**/*.m4v"]))
+      .use(timer("less"))
+      .use(less())
+      .use(timer("autoprefixer"))
+      .use(autoprefixer())
+      .use(timer("css move"))
+      .use(copy({
+          pattern: "assets/css/*.css",
+          move: false,
+          transform(file) {
+            return path.join(
+              ...path
+                .dirname(file)
+                .split(path.sep)
+                .slice(1),
+              "admin",
+              path.basename(file)
+            );
           }
-        ]
-      })
-    )
-    .use(timer("make feed"))
-    .use(
-      feed({
-        collection: "vids"
-      })
-    )
-    .use(timer("fix index.html paths"))
-    .use(
-      paths({
-        property: "paths",
-        directoryIndex: "index.html"
-      })
-    )
-    .use(timer("nunjucks templating"))
-    .use(
-      layouts({
-        // engine: "nunjucks",
-        // directory: "layouts",
-        pattern: "**/*.html",
-        engineOptions: {
-          filters: { date: nunjucksDate }
+        }))
+      // .use(
+      //   sharp({
+      //     src: "**/*.svg",
+      //     namingPattern: "{dir}{name}-og.jpg",
+      //     methods: [
+      //       {
+      //         name: "resize",
+      //         args: [1200, 630]
+      //       },
+      //       {
+      //         name: "toFormat",
+      //         args: ["jpeg"]
+      //       }
+      //     ],
+      //     moveFile: false
+      //   })
+      // )
+      .use(timer("move other assets"))
+      .use(copy({ pattern: "assets/**/*.*", move: true, transform(file) {
+            return path.join(...path
+                .dirname(file)
+                .split(path.sep)
+                .slice(1), path.basename(file));
+          } }))
+      .use(cleanCSS())
+      .use(timer("fingerprint"))
+      .use(fingerprint({ pattern: ["css/*.css", "js/*.js"] }))
+      .use(timer("admin css"))
+      .use(copy({ pattern: "css/admin/*.*", move: true, transform(file) {
+            return path.join("css", path.basename(file));
+          } }))
+      // .use(getFandoms())
+      .use(timer("admin config"))
+      .use(replace({
+          "admin/config.yml": {
+            find: /abcd/gi,
+            replace: () => `${nextId + 1}`
+          }
+        }))
+      .use(timer("rename md to basename version"))
+      .use(each((f, filename) => {
+          const file = f;
+          file.basename = path.basename(filename, ".md");
+          return filename;
+        }))
+      .use(ignore(["tags/**", "**/*.less"]))
+      .use(timer("copy vids to vidplayer"))
+      .use(copy({ pattern: "vids/*.*", move: false, transform(file) {
+            const newpath = path.join("vidplayer", ...path
+                .dirname(file)
+                .split(path.sep)
+                .slice(1), path.basename(file));
+            return newpath;
+          } }))
+      .use(timer("add markdown layout metadata"))
+      .use(fileMetadata([
+          { pattern: "*.md", metadata: { layout: "home.njk" } },
+          { pattern: "vids/**", metadata: { layout: "vid.njk" } },
+          { pattern: "vidplayer/**", metadata: { layout: "vidplayer.njk" } }
+        ]))
+      .use(timer("make vids collection"))
+      .use(collections({
+          vids: {
+            pattern: "vids/*.md",
+            sortBy: "date",
+            reverse: true
+          }
+        }))
+      .use(timer("transform md to html"))
+      .use(markdown())
+      .use(timer("permalinks"))
+      .use(permalinks({ // pattern: "post/:date/:title",
+          // date: "YYYY/MM/DD",
+
+          linksets: [{ match: { collection: "vids" }, pattern: "vid/:basename" }, { match: { collection: "vidplayer" }, pattern: "vidplayer/:basename" }] }))
+      .use(timer("make feed"))
+      .use(feed({ collection: "vids" }))
+      .use(timer("fix index.html paths"))
+      .use(paths({ property: "paths", directoryIndex: "index.html" }))
+      .use(timer("nunjucks templating"))
+      .use(layouts({ // engine: "nunjucks",
+          // directory: "layouts",
+          pattern: "**/*.html", engineOptions: { filters: { date: nunjucksDate } } }))
+      .use(msIf(process.env.CONTEXT === "production", subsetfonts())) // this plugin will run
+      .use(timer("inline svgs"))
+      .use(inlineSource({ swallowErrors: true }))
+      .use(timer("minify html"))
+      .use(htmlMinifier({
+          removeAttributeQuotes: false,
+          removeEmptyAttributes: false
+        }))
+      .use(timer("create sitemap"))
+      .use(sitemap({
+          hostname: sitedata.url,
+          omitIndex: true,
+          pattern: ["vid/**/*.html", "*.html"]
+        }))
+      .use(timer("google webmaster file"))
+      .use(copy({
+          pattern: "**/googlea7adbfb6a9a0f483",
+          move: true,
+          transform(file) {
+            return `${path.basename(file)}.html`;
+          }
+        }))
+      .build(err => {
+        if (err) {
+          reject(err);
         }
-      })
-    )
-    .use(
-      msIf(
-        process.env.CONTEXT === "production",
-        subsetfonts() // this plugin will run
-      )
-    )
-    .use(timer("inline svgs"))
-    .use(
-      inlineSource({
-        swallowErrors: true
-      })
-    )
-    .use(timer("minify html"))
-    .use(
-      htmlMinifier({
-        removeAttributeQuotes: false,
-        removeEmptyAttributes: false
-      })
-    )
-    .use(timer("create sitemap"))
-    .use(
-      sitemap({
-        hostname: sitedata.url,
-        omitIndex: true,
-        pattern: ["vid/**/*.html", "*.html"]
-      })
-    )
-    .use(timer("google webmaster file"))
-    .use(
-      copy({
-        pattern: "**/googlea7adbfb6a9a0f483",
-        move: true,
-        transform(file) {
-          return `${path.basename(file)}.html`;
-        }
-      })
-    )
-    .build(err => {
-      if (err) {
-        reject(err)
-      }
-      resolve();
-    });
+        resolve();
+      });
   })
   
 }
---
+++ src/vids/068-Twilight.md
@@ -0,0 +1,32 @@
+---
+title: 'Twilight'
+date: 2018-02-18T15:38:39.000Z
+published: true
+description: 'for se42'
+lyrics: ""
+footage:
+    - 'The Neverending Story (1984)'
+song: 'Prologue, Twilight'
+artist: 'Electric Light Orchestra'
+song_info_url: 'https://en.wikipedia.org/wiki/Twilight_(Electric_Light_Orchestra_song)'
+downloads:
+    - {url: AbsoluteDestiny-Twilight-480p.m4v, title: '480p mp4', width: 1152, height: 480, mimetype: video/mp4}
+    - {url: AbsoluteDestiny-Twilight-720p.m4v, title: '720p mp4', width: 1728, height: 720, mimetype: video/mp4}
+    - {url: AbsoluteDestiny-Twilight-1080p.m4v, title: '1080p mp4', width: 1920, height: 800, mimetype: video/mp4}
+srt: null
+sd_stream: AbsoluteDestiny-Twilight-720p.m4v
+svg: AbsoluteDestiny-Twilight.svg
+bgimg: AbsoluteDestiny-Twilight.jpg
+hd_stream: AbsoluteDestiny-Twilight-1080p.m4v
+tags:
+    - gift
+    - spoilers
+    - liveaction
+vid_id: '068'
+mux: HA02mxY02eF5r8vnRYryWdWDvXRXZB2WSR
+yt_id: EdXbGmjiwQA
+pw: 1920
+ph: 800
+
+---
+for Jetpack Monkey
